ca01e053e9219dc63c7ab10ecb41e4c3b98b1adf
Kloss95 <111370618+Kloss95@users.noreply.github.com>
2026-06-26 19:15:09 +0200

    Add files via upload

--- public/main.js
+++ public/main.js
@@ -3,7 +3,7 @@ function openGame(gameUrl) {
     const modal = document.getElementById('game-modal');
     const iframe = document.getElementById('game-frame');
     
-    // Si la modale n'existe pas dans le HTML, on affiche une erreur dans la console
+    // Sécurité : si la modale n'est pas trouvée
     if (!modal || !iframe) {
         console.error("ERREUR : La structure HTML de la modale est introuvable !");
         return;
@@ -12,7 +12,7 @@ function openGame(gameUrl) {
     // On indique à l'iframe d'aller chercher le fichier du jeu
     iframe.src = gameUrl;
     
-    // On affiche la fenêtre
+    // On affiche la fenêtre en grand
     modal.style.display = 'flex';
 }
 
@@ -22,7 +22,7 @@ function closeGame() {
     const iframe = document.getElementById('game-frame');
     
     if (iframe) {
-        // On vide la source de l'iframe pour couper le jeu et le son
+        // On vide la source de l'iframe pour couper le jeu et le son instantanément
         iframe.src = '';
     }
     if (modal) {
